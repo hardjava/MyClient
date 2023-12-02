f58f8895d1f567ea9f86f29dadb880e1919ce83f
Song <fkdlcn123@naver.com>
2023-12-03 02:35:22 +0900

    commit

--- src/main/java/airbnb/persistence/dto/HouseAndReservationDTO.java
+++ src/main/java/airbnb/persistence/dto/HouseAndReservationDTO.java
@@ -1,6 +1,5 @@
 package airbnb.persistence.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,10 +12,10 @@ import java.sql.Date;
 @Builder
 public class HouseAndReservationDTO implements Serializable {
     private String houseName;
-    int reservationId, guestNum;
-    Date checkIn, checkOut;
-    int cost, userId;
-    String loginId, userName, userPhone;
+    private int reservationId, guestNum;
+    private Date checkIn, checkOut;
+    private int cost, userId;
+    private String loginId, userName, userPhone;
 
     public HouseAndReservationDTO(String houseName, int reservationId, int guestNum, Date checkIn, Date checkOut, int cost, int userId, String loginId, String userName, String userPhone) {
         this.houseName = houseName;
@@ -33,7 +32,6 @@ public class HouseAndReservationDTO implements Serializable {
 
     public String toString() {
         return String.format("%-30s%-15s%-15s%-15d%-15s%-15s%-30s%-30d", houseName, checkIn,
-                checkOut, guestNum, userName,
-                userPhone, loginId, cost);
+                checkOut, guestNum, userName, userPhone, loginId, cost);
     }
 }
